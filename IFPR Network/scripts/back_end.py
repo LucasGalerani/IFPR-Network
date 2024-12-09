c6fe721f8e0ba9d8daf1b76802fd10ab4bc724ab
Lucas de Souza Galerani <lucas1501010@gmail.com>
2024-12-09 19:33:29 -0300

    Ajustes no Script que usa a conexao do banco de dados

--- IFPR Network/scripts/back_end.py
+++ IFPR Network/scripts/back_end.py
@@ -1,18 +1,18 @@
-from scripts.conexao import Conexao
 import hashlib
+from scripts.conexao import Conexao
 
 def hashConvertor(text):
     hash_obj = hashlib.sha256(text.encode())
-    hash_resultado = hash_obj.hexdigest()
-    return hash_resultado
+    return hash_obj.hexdigest()
 
 def validar_dados(email, senha):
     hashSenha = hashConvertor(senha)
-    minha_conexao = Conexao()
-    query = "SELECT * FROM usuario WHERE email = %s AND senha = %s"
-    minha_conexao.cursor.execute(query, (email, hashSenha))
-    resultado = minha_conexao.cursor.fetchone()
-    if resultado is None:
+    try:
+        with Conexao() as minha_conexao:
+            query = "SELECT * FROM usuario WHERE email = %s AND senha = %s"
+            minha_conexao.cursor_dict.execute(query, (email, hashSenha))
+            resultado = minha_conexao.cursor_dict.fetchone()
+            return resultado is not None
+    except Exception as e:
+        print(f"Erro ao validar dados: {e}")
         return False
-    else:
-        return True
